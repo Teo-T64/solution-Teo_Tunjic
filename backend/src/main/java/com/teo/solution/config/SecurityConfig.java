a3871c19a0b6ff47de01f98dd7f6d4b17be24dcd
Teo <teotunjic64@gmail.com>
2026-07-03 23:31:09 +0200

    app status check added

--- backend/src/main/java/com/teo/solution/config/SecurityConfig.java
+++ backend/src/main/java/com/teo/solution/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/**").permitAll()
+                        .requestMatchers("/api/**","/status","/health").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
